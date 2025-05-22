8442b90023a09c363b493673a8492c4036376939
DW <kontakt.danilwisniewski@gmail.com>
2025-05-22 19:00:32 +0200

    update closed chat button position

--- frontend/app/(routes)/chat/[llmProvider]/[model]/[vectorstore]/page.tsx
+++ frontend/app/(routes)/chat/[llmProvider]/[model]/[vectorstore]/page.tsx
@@ -22,9 +22,9 @@ const Chat = () => {
 
   return (
     <div
-      className={`fixed bottom-5 right-5 ${
-        isOpen ? "w-auto h-auto" : "w-[100px] h-[100px]"
-      }`}
+      className={`fixed ${
+        isOpen ? "bottom-5 right-5" : "bottom-0 right-0"
+      } bottom-5 right-5 ${isOpen ? "w-auto h-auto" : "w-[100px] h-[100px]"}`}
     >
       <ChatWindow isOpen={isOpen} onClose={handleClose} />
       <OpenChatButton setIsOpen={setIsOpen} isOpen={isOpen} />
